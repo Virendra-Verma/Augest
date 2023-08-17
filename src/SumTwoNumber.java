3bf564ae3fd1f0abebd68bb46bbe78690ad1363f
Virendra-Verma <vkverma991099@gmail.com>
2023-08-18 00:26:51 +0530

    Initial commit

---
+++ src/SumTwoNumber.java
@@ -0,0 +1,8 @@
+public class SumTwoNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 8;
+        int sum = a+b;
+        System.out.println("Result : " + sum);
+    }
+}
