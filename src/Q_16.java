dcd52c9268deebfac62d47b3606a7668c49ba70e
Virendra-Verma <vkverma991099@gmail.com>
2023-08-21 01:23:26 +0530

    Initial commit

---
+++ src/Q_16.java
@@ -0,0 +1,17 @@
+public class Q_16 {
+    public static void Number_Pyramid(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Number_Pyramid(5);
+
+    }
+}
