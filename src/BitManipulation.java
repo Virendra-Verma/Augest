ecbb118747d2b7e6718d4ae703d6f2ad36b6334f
Virendra-Verma <vkverma991099@gmail.com>
2023-09-20 22:11:43 +0530

    Initial commit

---
+++ src/BitManipulation.java
@@ -0,0 +1,9 @@
+public class BitManipulation {
+    public static void main(String[] args) {
+        System.out.println("The AND operator of 5 and 6 : "+ (5&6));
+        System.out.println("The OR operator of 5 and 6 : "+ (5|6));
+        System.out.println("The XOR operator of 5 and 6 : "+ (5^6));
+        System.out.println("The NOT operator of 5  : "+ ~5);
+
+    }
+}
